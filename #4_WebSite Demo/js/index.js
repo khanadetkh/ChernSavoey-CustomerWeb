70c57145669109a0ad513a7209d416b82c66c1a6
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-04-28 00:37:14 +0700

    update login wiht Google Account

---
+++ #4_WebSite Demo/js/index.js
@@ -0,0 +1,40 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDiLRSKZJU8F5UhTFHmMyHa0qB1CAzMuLw",
+  authDomain: "it60-42-choen-savoey.firebaseapp.com",
+  databaseURL: "https://it60-42-choen-savoey.firebaseio.com",
+  projectId: "it60-42-choen-savoey",
+  storageBucket: "it60-42-choen-savoey.appspot.com",
+  messagingSenderId: "208922727243",
+  appId: "1:208922727243:web:45cb03e5d2eebb8a948ece",
+  measurementId: "G-0KGMXHYKEJ"
+};
+firebase.initializeApp(firebaseConfig);
+
+
+
+function Googlelogin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    window.location = "app.html";
+    // ...
+  }).catch(function (error) {
+    var errorMessage = error.message;
+    alert(errorMessage);
+  });
+
+}
+
+
+
+
+var login = function () {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  if (username === 'bob' && password === 'secret') {
+    ons.notification.alert('Congratulations!');
+  } else {
+    ons.notification.alert('Incorrect username or password.');
+  }
+};
