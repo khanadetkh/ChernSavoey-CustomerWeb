5d2c98e12781a0ca19e1dcf26418e82905468d91
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-05-01 01:16:50 +0700

    Commit ไปก่อง ไม่รู้อะไร 555

--- #4_WebSite Demo/js/main.js
+++ #4_WebSite Demo/js/main.js
@@ -21,29 +21,29 @@ window.fn.pushPage = function (page, anim) {
   }
 };
 
- // Initialize Firebase
-      const firebaseConfig = {
-        apiKey: "AIzaSyDiLRSKZJU8F5UhTFHmMyHa0qB1CAzMuLw",
-        authDomain: "it60-42-choen-savoey.firebaseapp.com",
-        databaseURL: "https://it60-42-choen-savoey.firebaseio.com",
-        projectId: "it60-42-choen-savoey",
-        storageBucket: "it60-42-choen-savoey.appspot.com",
-        messagingSenderId: "208922727243",
-        appId: "1:208922727243:web:45cb03e5d2eebb8a948ece",
-        measurementId: "G-0KGMXHYKEJ"
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDiLRSKZJU8F5UhTFHmMyHa0qB1CAzMuLw",
+  authDomain: "it60-42-choen-savoey.firebaseapp.com",
+  databaseURL: "https://it60-42-choen-savoey.firebaseio.com",
+  projectId: "it60-42-choen-savoey",
+  storageBucket: "it60-42-choen-savoey.appspot.com",
+  messagingSenderId: "208922727243",
+  appId: "1:208922727243:web:45cb03e5d2eebb8a948ece",
+  measurementId: "G-0KGMXHYKEJ"
+};
+firebase.initializeApp(firebaseConfig);
+
+
+//form onsen UI
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'home.html') {
+    page.querySelector('#push-button').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
     };
-    firebase.initializeApp(firebaseConfig);
-
-
-
-function Googlelogin() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        window.location = "login.html";
-        // ...
-      }).catch(function(error) {
-        var errorMessage = error.message;
-alert(errorMessage);
-      });
-
-}
+  } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
