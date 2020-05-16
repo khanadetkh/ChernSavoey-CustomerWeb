397220ee8fe8ef2ee5261275f37a5e5ab7586518
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-05-16 23:06:56 +0700

    #Release1 - GG_Login !!complete

---
+++ #Release 1/js/index.js
@@ -0,0 +1,119 @@
+//Firebase Auth
+const firebaseConfig = {
+    apiKey: "AIzaSyDiLRSKZJU8F5UhTFHmMyHa0qB1CAzMuLw",
+    authDomain: "it60-42-choen-savoey.firebaseapp.com",
+    databaseURL: "https://it60-42-choen-savoey.firebaseio.com",
+    projectId: "it60-42-choen-savoey",
+    storageBucket: "it60-42-choen-savoey.appspot.com",
+    messagingSenderId: "208922727243",
+    appId: "1:208922727243:web:45cb03e5d2eebb8a948ece",
+    measurementId: "G-0KGMXHYKEJ"
+  };
+  firebase.initializeApp(firebaseConfig);
+  
+  
+  
+  function Googlelogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      window.location = "app.html";
+      // ...
+    }).catch(function (error) {
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+  
+  }
+  
+  //pure-login
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).find('i').removeClass('zmdi-eye');
+            $(this).find('i').addClass('zmdi-eye-off');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).find('i').addClass('zmdi-eye');
+            $(this).find('i').removeClass('zmdi-eye-off');
+            showPass = 0;
+        }
+        
+    });
+
+
+})(jQuery);
+
+
+  
