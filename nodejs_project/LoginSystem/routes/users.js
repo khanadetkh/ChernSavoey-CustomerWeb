da15915f6d3829f3f5cbc0c3954ec23e5fbeb47d
Pomsirirat <35446103+Pomsirirat@users.noreply.github.com>
2020-08-14 16:22:25 +0700

    Update users.js

--- nodejs_project/LoginSystem/routes/users.js
+++ nodejs_project/LoginSystem/routes/users.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-<<<<<<< HEAD
+
 router.get('/register', function (req, res, next) {
   res.send('register');
 });
@@ -13,12 +13,3 @@ router.get('/login', function (req, res, next) {
   res.send('login ');
 });
 module.exports = router;
-=======
-router.get('/register', function(req, res, next) {
-  res.render('register');
-});
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
-module.exports = router;
->>>>>>> cbb49f04d4d2a87fca1b375df31e6cde7c42d483
