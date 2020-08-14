e6d9b4827fccfa1470ddf625f01e93fcbfebca0a
Pomsirirat <35446103+Pomsirirat@users.noreply.github.com>
2020-08-14 16:11:08 +0700

    back to last version
    กลับไปเป็น version ที่ใช้ localhost:3000/users/register-login ได้

--- nodejs_project/LoginSystem/routes/users.js
+++ nodejs_project/LoginSystem/routes/users.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-
+<<<<<<< HEAD
+router.get('/register', function (req, res, next) {
+  res.send('register');
+});
+router.get('/login', function (req, res, next) {
+  res.send('login ');
+});
 module.exports = router;
+=======
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+module.exports = router;
+>>>>>>> cbb49f04d4d2a87fca1b375df31e6cde7c42d483
