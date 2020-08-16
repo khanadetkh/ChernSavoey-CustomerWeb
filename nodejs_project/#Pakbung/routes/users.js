a639ef5120ef86ef479eb27bb48b0bd54aafee88
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-08-17 01:24:51 +0700

    update login fn.

---
+++ nodejs_project/#Pakbung/routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register');
+
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+router.post('/register', function (req, res, next) {
+  console.log(req.body.name);
+});
+
+
+module.exports = router;
