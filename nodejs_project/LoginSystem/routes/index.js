0152d7b55983f76c5e6e28be8eac903677036c12
Pomsirirat <35446103+Pomsirirat@users.noreply.github.com>
2020-08-19 11:46:30 +0700

    Update index.js

--- nodejs_project/LoginSystem/routes/index.js
+++ nodejs_project/LoginSystem/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
