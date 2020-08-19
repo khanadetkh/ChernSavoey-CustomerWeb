0246c1d885969b9f5d8010e89bb34fbd6f329a85
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-08-19 11:45:58 +0700

    push to login sys.

--- nodejs_project/LoginSystem/routes/users.js
+++ nodejs_project/LoginSystem/routes/users.js
@@ -1,14 +1,50 @@
-var express = require('express');
+// -- Router
+var express = require("express");
 var router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
-router.get('/register', function(req, res, next) {
-  res.send('register');
+
+router.get("/register", function (req, res, next) {
+  res.render("register");
 });
-router.get('/login', function(req, res, next) {
-  res.send('login ');
+
+router.get("/login", function (req, res, next) {
+  res.render("login");
 });
+
+router.post('/register', [
+  check('email', 'กรุณาป้อนอีเมล').isEmail(),
+  check('name', 'กรุณาป้อนชื่อของท่าน').not().isEmpty(),
+  check('password', 'กรุณาป้อนรหัสผ่าน').not().isEmpty()
+], function(req, res, next) {
+  const result = validationResult(req);
+  var errors = result.errors;
+  //Validation Data
+  if (!result.isEmpty()) {
+    //Return error to views
+    res.render('register', {
+      errors: errors
+    })
+  } else {
+    //Insert  Data
+    var name = req.body.name;
+    var password = req.body.password;
+    var email = req.body.email;
+    var newUser = new User({
+      name: name,
+      password: password,
+      email: email
+    });
+    User.createUser(newUser, function(err, user) {
+      if (err) throw err
+    });
+    res.location('/');
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
