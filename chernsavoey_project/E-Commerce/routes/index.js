39227882eb90bb292c853522cbe6f50bb727ee00
variin <35445635+variin@users.noreply.github.com>
2020-08-24 16:47:34 +0700

    เพิ่มหน้า cart & link หน้า
    เพิ่มหน้า cart & link หน้า

--- chernsavoey_project/E-Commerce/routes/index.js
+++ chernsavoey_project/E-Commerce/routes/index.js
@@ -3,19 +3,27 @@ const shop = require('../model/shop');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/',async function(req, res, next) {
- const result = await shop.displayShopName();
- console.log(result);
+router.get('/', async function (req, res, next) {
+   const result = await shop.displayShopName();
+   console.log(result);
 
- const result2 = await shop.displayCategory();
- console.log(result2);
+   const result2 = await shop.displayCategory();
+   console.log(result2);
 
- res.render('index')
+   res.render('index')
 });
 
-router.get('/cart',async function(req, res, next) {
+router.get('/cart', async function (req, res, next) {
    res.render('cart');
-   });
+});
 
-   
+router.get('/menu', async function (req, res, next) {
+   res.render('menu');
+});
+
+router.get('/', async function (req, res, next) {
+   res.render('index');
+});
+
+
 module.exports = router;
