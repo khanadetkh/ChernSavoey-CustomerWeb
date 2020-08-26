9682f792eccd0af841efd91c3fd6de60b4df797a
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-08-26 11:42:51 +0700

    revert

--- chernsavoey_project/Pakbung/model/user.js
+++ chernsavoey_project/Pakbung/model/user.js
@@ -9,66 +9,16 @@ const userLogin = async function () {
     return result
 }
 // <--สร้างฟังก์ชั่น Register-->
-<<<<<<< HEAD
 const userRegister = async function (name,password,email) {
   console.log(name,password,email);
-  var User = module.exports = db.collection('users');
-    const userSchema = await User.add({
+    const docRef = db.collection('users');
+    const result = await docRef.add({
         username: name,
         password: password,
         email: email
       });
-    return userSchema
-    
-=======
-const userRegister = async function () {
-    const docRef = db.collection('users').doc('D7giGLR2cLUXoj7aWyVA');
-    // const result = await docRef.add({
-    //     username: name,
-    //     password: password,
-    //     email: email
-    //   });
-    const data = {
-        stringExample: name.,
-        booleanExample: true,
-        numberExample: 3.14159265,
-        arrayExample: [5, true, 'hello'],
-        nullExample: null,
-        objectExample: {
-          a: 5,
-          b: true
-        }
-      };
-      
-      const result = await docRef.set(data);
-      
     return result
->>>>>>> parent of d16aa1a3... [improve] Register System
-}
-
-module.exports.createUser = async function(newUser, callback) {
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(newUser.password, salt, function(err, hash) {
-      newUser.password = hash;
-      newUser.save(callback);
-    });
-  });
-}
-
-module.exports.getUserById = async function(id, callback) {
-  User.findById(id, callback);
-}
-module.exports.getUserByName = async function(name, callback) {
-  var query = {
-    name: name
-  };
-  User.findOne(query, callback);
-}
-
-module.exports.comparePassword = async function(password, hash, callback) {
-  bcrypt.compare(password, hash, async function(err, isMatch) {
-        callback(null, isMatch);
-  });
+    
 }
 
 module.exports = {
--- chernsavoey_project/Pakbung/routes/users.js
+++ chernsavoey_project/Pakbung/routes/users.js
@@ -1,35 +1,27 @@
 // -- Router
 var express = require("express");
 var router = express.Router();
-const User = require('../model/user');
+const user = require('../model/user');
 const { check, validationResult } = require("express-validator");
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
-
-  const result = await user.userLogin();
-  console.log(result);
-
   res.send("respond with a resource");
 });
 
 router.get("/register", async function (req, res, next) {
-  const result = await user.userRegister();
-  console.log(result);
-
   res.render("register");
 });
 
-router.get("/login", async function (req, res, next) {
+router.get("/login", function (req, res, next) {
   res.render("login");
 });
 
-// Register function
 router.post('/register', [
   check('email', 'กรุณาป้อนอีเมล').isEmail(),
   check('name', 'กรุณาป้อนชื่อของท่าน').not().isEmpty(),
   check('password', 'กรุณาป้อนรหัสผ่าน').not().isEmpty()
-], function (req, res, next) {
+], async function (req, res, next) {
   const result = validationResult(req);
   var errors = result.errors;
   //Validation Data
@@ -43,40 +35,14 @@ router.post('/register', [
     var name = req.body.name;
     var password = req.body.password;
     var email = req.body.email;
-    var newUser = new User({
-      name: name,
-      password: password,
-      email: email
-    });
-<<<<<<< HEAD:chernsavoey_project/Pakbung/controller/users.js
-    await User.createUser(newUser, async function(err, user) {
-      if (err) throw err
-    });
-
-    // const userRegis = await user.userRegister(name,password,email);
-    // if (!userRegis) {
-    //   res.render('register', {
-    //     errors:[{msg :  "can't register"}]
-    //   })
-    // }
-=======
-    User.createUser(newUser, function (err, user) {
-      if (err) throw err
-    });
-    res.location('/');
->>>>>>> parent of d16aa1a3... [improve] Register System:chernsavoey_project/Pakbung/routes/users.js
+    const userRegis = await user.userRegister(name,password,email);
+    if (!userRegis) {
+      res.render('register', {
+        errors:[{msg :  "can't register"}]
+      })
+    }
     res.redirect('/');
   }
 });
-
-// Login function
-// router.post('/login', passport.authenticate('local', {
-//   failureRedirect: '/users/login',
-//   failureFlash: true
-// }),
-// function(req, res) {
-//       req.flash("success", "ลงชื่อเข้าใช้เรียบร้อยแล้ว");
-//       res.redirect('/');
-// });
 
 module.exports = router;
