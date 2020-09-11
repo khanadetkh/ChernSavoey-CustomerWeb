b95c4dc031d60c9641215ccaf910aba94384124e
variin <35445635+variin@users.noreply.github.com>
2020-09-11 14:29:06 +0700

    update index
    -

--- chernsavoey_project/E-Commerce/model/shop.js
+++ chernsavoey_project/E-Commerce/model/shop.js
@@ -8,7 +8,7 @@ const db = require('./db');
 
 const displayShopName = async function(){
     const storeList = [];
-       const docRef = db.collection('Store').doc('gixXindVOCKg6KqqD9MY');
+       const docRef = db.collection('store').doc('gixXindVOCKg6KqqD9MY');
        const result =  await docRef.get().then(doc => {
                storeList.push(doc.data());
            });
@@ -23,7 +23,7 @@ const displayCategory = async function(){
 }
 
 const getAllShop = async function(){
-    const docRef = db.collection('Store')
+    const docRef = db.collection('store')
     const result = await docRef.get()
     return result
 }
