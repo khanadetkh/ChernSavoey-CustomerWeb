17c1aa3aec10ef9c4757861667cf7308866084cf
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-09-17 01:25:30 +0700

    [+] menu list

--- chernsavoey_project/chernsavoey/routes/menusController.js
+++ chernsavoey_project/chernsavoey/routes/menusController.js
@@ -3,21 +3,7 @@ const db = require('../model/db');
 
 var router = express.Router();
 
-// router.get('/:storeName', async function (req, res, next) {
-//    let shopId = req.params.storeId;
 
-//    const shopDetail = await db
-//       .collection('store')
-//       .doc(shopId).get().then((querySnapshot) => querySnapshot.data());
-// });
-
-// const menu = await db.collection('menu').where('storeName', '==', storeName)
-//    .orderBy("name", "asc")
-//    .get()
-//    .then
-//    (
-
-//    )
 
 
 module.exports = router;
