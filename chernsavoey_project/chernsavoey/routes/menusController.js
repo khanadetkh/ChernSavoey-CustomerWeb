2f572e7e088d28c57b317d532f01dd33263b9dc4
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-09-20 02:51:52 +0700

    [edit] path

--- chernsavoey_project/chernsavoey/routes/menusController.js
+++ chernsavoey_project/chernsavoey/routes/menusController.js
@@ -3,6 +3,34 @@ const db = require('../model/db');
 
 var router = express.Router();
 
+router.get("/:storeId", async (req, res) => {
+	const storeId = req.params.storeId;
+	const categoryId = req.params.categoryId;
+
+	const menuDetails = await db.collection("store")
+		.doc(storeId)
+		.get()
+		.then((querySnapshot) => querySnapshot.data());
+
+	const shopName = menuDetails.storeName;
+	let menuList = menuDetails.menu;
+	const categoriesList = menuDetails.categories;
+	let categoriesFilter = [];
+	if (categoryId) {
+		// categoriesFilter = menuDetails.categories.filter((item) => item.category == categoryId)
+		menuList = menuList.filter((item) => item.category == categoryId);
+	}
+	console.log(storeId);
+	console.log(shopName);
+	console.log(menuList);
+	console.log(categoriesList);
+	console.log(categoriesFilter);
+
+
+	res.render("menu", { storeId,shopName, menuList, categoriesList });
+}
+);
+
 router.get("/:storeId/:categoryId", async (req, res) => {
 	const storeId = req.params.storeId;
 	const categoryId = req.params.categoryId;
@@ -20,13 +48,14 @@ router.get("/:storeId/:categoryId", async (req, res) => {
 		// categoriesFilter = menuDetails.categories.filter((item) => item.category == categoryId)
 		menuList = menuList.filter((item) => item.category == categoryId);
 	}
+	console.log(storeId);
 	console.log(shopName);
 	console.log(menuList);
 	console.log(categoriesList);
 	console.log(categoriesFilter);
 
 
-	res.render("menu", { shopName, menuList, categoriesList });
+	res.render("menu", { storeId,shopName, menuList, categoriesList });
 }
 );
 
