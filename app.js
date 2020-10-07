ce76ff18d2f3dca193adea8380b7447aed66e9a0
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-10-08 01:37:15 +0700

    add user profile + google login

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
 const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {console.log(`App running on port ${PORT}`)})
+const session = require('express-session');
 
-var session = require('express-session');
-var passport=require('passport');
-var LocalStrategy=require('passport-local').Strategy;
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 //ดึง controller มาใช้
-var indexRouter = require('./routes/usersController');
-var usersRouter = require('./routes/usersController');
 var shopsRouter = require('./routes/shopsController');
 var orderListRouter = require('./routes/orderListController');
 var inboxRouter = require('./routes/inboxController');
 var chatRouter = require('./routes/chatController');
-var profileRouter = require('./routes/profileController');
 var editProfileRouter = require('./routes/editProfileController');
 var cartRouter = require('./routes/cartController');
 var orderSenderRouter = require('./routes/orderSenderController');
@@ -27,13 +30,10 @@ var homeSenderRouter = require('./routes/homeSenderController');
 
 
 //กำหนดตัวแปรให้ controller
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/shops', shopsRouter);
 app.use('/orderList', orderListRouter);
 app.use('/inbox', inboxRouter);
 app.use('/chat', chatRouter);
-app.use('/profile', profileRouter);
 app.use('/editProfile', editProfileRouter);
 app.use('/cart', cartRouter);
 app.use('/orderSender', orderSenderRouter);
@@ -52,48 +52,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.locals.descriptionText = function(text, length) {
-  return text.substring(0, length);
-}
-
-app.locals.formatMoney = function(number) {
-  return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-}
-
-app.use(express.static('public'))
-
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true,
-}));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-app.use(require('connect-flash')());
-
-// เรียก port
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`)
-})
-
-// socket.io
-// io.on('connection', function (socket) {
-//   console.log('a user connected')
-// })
-
-app.get('*',async function(req,res,next){
-    res.locals.user = req.user || null;
-    next();
-});
-
-
-// catch 404 and 500 forward to error handler
-app.use(async function (req, res, next) {
-  next(createError(404));
-});
-
 // error handler
 app.use(async function (err, req, res, next) {
   // set locals, only providing error in development
@@ -104,5 +62,70 @@ app.use(async function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('500error');
 });
+
+
+// 
+
+app.get('/', function(req, res) {
+  res.render('login');
+});
+
+var passport = require('passport');
+var userProfile;
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+app.get('/profile', (req, res) => {
+  res.render('profile', {user: userProfile});
+  console.log(userProfile);
+});
+app.get('/error', (req, res) => res.send("error logging in"));
+ 
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+ 
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+/*  Google AUTH  */
+ 
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '208922727243-chcjrc4uu520omqom1csgobhagoli40i.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = 'clU0mrAKbXhmzPl2ONsu1S3q';
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/shops');
+  });
+
+// route for logging out
+app.get('/logout', function(req, res) {
+  req.session.destroy(function(err){
+      req.logout();
+      res.redirect('/');
+  });
+});
+
+
 
 module.exports = app;
