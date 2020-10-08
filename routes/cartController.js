108756be4088b26428f8d6ec6a3c4a7ea0c9fd10
variin <35445635+variin@users.noreply.github.com>
2020-10-08 17:52:24 +0700

    cart.ejs
    show location & note

--- routes/cartController.js
+++ routes/cartController.js
@@ -3,47 +3,49 @@ const db = require('../model/db');
 var router = express.Router();
 
 /* GET shops page. */
-router.get('/', async function (req, res, next) {
+router.
+get('/', async function (req, res, next) {
     res.render('cart');
 });
 
-router.post('/orderDetails', async (req, res) => {
-    const menuArr = req.body.menus;
 
-    const details = [];
-    for (let i = 0; i < menuArr.length; i++) {
-        const menuId = menuArr[i];
+// router.post('/orderDetails', async (req, res) => {
+//     const menuArr = req.body.menus;
 
-        //  let menuDetails = await db.collection("menu").add(menuDetails);
-        let menuDetails = await db
-            .collection("menu")
-            .doc(menuId)
-            .get()
-            .then((querySnapshot) => querySnapshot.data());
+//     const details = [];
+//     for (let i = 0; i < menuArr.length; i++) {
+//         const menuId = menuArr[i];
+
+//         //  let menuDetails = await db.collection("menu").add(menuDetails);
+//         let menuDetails = await db
+//             .collection("menu")
+//             .doc(menuId)
+//             .get()
+//             .then((querySnapshot) => querySnapshot.data());
 
 
-        const storeId = menuDetails.store.id;
+//         const storeId = menuDetails.store.id;
 
-        menuDetails = {
-            id: menuId,
-            name: menuDetails.menuName,
-            price: menuDetails.price
-        };
+//         menuDetails = {
+//             id: menuId,
+//             name: menuDetails.menuName,
+//             price: menuDetails.price
+//         };
 
-        let storeDetails = await db
-            .collection("store")
-            .doc(storeId)
-            .get()
-            .then((querySnapshot) => querySnapshot.data());
+//         let storeDetails = await db
+//             .collection("store")
+//             .doc(storeId)
+//             .get()
+//             .then((querySnapshot) => querySnapshot.data());
 
-        storeDetails = { name: storeDetails.storeName };
+//         storeDetails = { name: storeDetails.storeName };
 
-        details.push({ menuDetails, storeDetails });
-    }
+//         details.push({ menuDetails, storeDetails });
+//     }
 
-    res.status(200).send({ "menuStoreDetails": details });
-}
-);
+//     res.status(200).send({ "menuStoreDetails": details });
+// }
+// );
 
 
 
