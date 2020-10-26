13e6d929192acb27be5b7cf6748fea69472f9d0c
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-10-26 17:23:40 +0700

    ++

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const socket = require('socket.io');
 const session = require('express-session');
+const bodyParser = require('body-parser')
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const passport = require('passport');
 
 const app = express();
+
+//use bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({
+//     limit: '50mb',
+//     extended: true,
+//     parameterLimit: 50000
+// }));
+
 
 /*Filter Server and Require for Socket io*/
 const PORT = process.env.PORT || 8080
