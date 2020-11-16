f4cc086cd0b598b6bceafded1208e9f2bf2455e9
variin <35445635+variin@users.noreply.github.com>
2020-11-16 20:02:37 +0700

    แก้path&เพิ่มหน้าNo order

--- app.js
+++ app.js
@@ -106,11 +106,11 @@ const chatSenderRouter = require("./routes/chatSenderController");
 const homeSenderRouter = require("./routes/homeSenderController");
 const updateStatusRouter = require("./routes/updateStatus_SenderController");
 const endpoints = require("./routes/endpoints.js");
-const myOrderSender = require("./routes/myOrderController");
+const myOrder = require("./routes/myOrderController");
 
 //กำหนดตัวแปรให้ controller
 app.use("/shops", isLoggedIn, shopsRouter);
-app.use("/orderList", isLoggedIn, orderListRouter);
+app.use("/orderList", isLoggedIn, orderListRouter); //ฝั่งคนส่ง for sender
 app.use("/inbox", isLoggedIn, inboxRouter);
 app.use("/chat", isLoggedIn, chatRouter);
 app.use("/orderSender", isLoggedIn, orderSenderRouter);
@@ -118,7 +118,7 @@ app.use("/inboxSender", isLoggedIn, inboxSenderRouter);
 app.use("/chatSender", isLoggedIn, chatSenderRouter);
 app.use("/sender", isLoggedIn, homeSenderRouter);
 app.use("/updateStatus_Sender", isLoggedIn, updateStatusRouter);
-app.use("/myOrder", isLoggedIn, myOrderSender)
+app.use("/myOrder", isLoggedIn, myOrder); //ฝั่งคนสั่ง for customer
 app.use("/endpoints", endpoints);
 
 // error handler
