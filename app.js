91121d8c7c628dbd00222923ece693b28ede5dd5
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-11-20 00:15:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -121,16 +121,22 @@ app.use("/updateStatus_Sender", isLoggedIn, updateStatusRouter);
 app.use("/myOrder", isLoggedIn, myOrder); //ฝั่งคนสั่ง for customer
 app.use("/endpoints", endpoints);
 
+
 // error handler
-app.use(async function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
     // render the error page
-    res.status(err.status || 500);
-    res.render("500error");
-});
+    if (err) {
+      console.log("APP Error =>>>>> ",err);
+      res.status(err.status || 500);
+      res.render('500error');
+  
+    } 
+    next()
+  
+  });
 
 
 
